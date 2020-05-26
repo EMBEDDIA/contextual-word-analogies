6baa04c926b472fb6acd3179c0aeaecfb45145a6
MatejUlcar <matej.ulcar@protonmail.com>
2020-05-26 16:15:57 +0200

    License update

--- elmo_analogies.sh
+++ elmo_analogies.sh
@@ -16,18 +16,3 @@ python3 elmo_layer0/get_layer0_embs.py --weights $weights --options $options --v
 layer0=${vocab}.emb.layer0
 python3 elmo_analogies.py --weights $weights --options $options --lang $lang --embeddings $layer0 2> log.txt
 
-
-# Copyright (C) 2019  Matej Ulčar <matej.ulcar@fri.uni-lj.si>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
